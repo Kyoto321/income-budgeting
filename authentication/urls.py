b92df164b555b39c89b508c62c4809cf794e03ff
Kyoto321 <bankoleayodele147@gmail.com>
2024-03-07 16:21:40 +0100

    create reset-password-link

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmail, LoginAPIView
+from .views import (
+    RegisterView, 
+    VerifyEmail, 
+    LoginAPIView, 
+    PasswordTokenCheck, 
+    RequestPasswordReset,
+    SetNewPassword
+)
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
@@ -11,6 +18,10 @@ urlpatterns = [
     path('email-verify/', VerifyEmail.as_view(), name='email-verify'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('request-password-reset/', RequestPasswordReset.as_view(), name='request-password-reset'),
+    path('password-reset/<uidb64>/<token>/', PasswordTokenCheck.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete', SetNewPassword.as_view(), name='password-reset-complete')
+    
 ]
 
  
